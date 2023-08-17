a624acc88ae5676036d2af94a7cde52a2b649618
budou114 <58349030+budou114@users.noreply.github.com>
2023-08-17 15:40:34 +0900

    ルーティングの学習

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+
   @RequestMapping("/")
   public String index() {
     return "index";
+  }
+
+  @RequestMapping("/{temp}")
+  public String other(@PathVariable String temp) {
+    switch (temp) {
+      case "index":
+        return "index";
+
+      default:
+        return "other";
+    }
   }
 }
